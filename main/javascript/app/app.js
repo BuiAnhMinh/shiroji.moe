086d98c2499706d416dedc46ba1b11b56eb3a24d
buianhminh <bui.anh.minh99@gmail.com>
2024-04-20 00:49:06 +1000

    create classes and have fun with API

--- main/javascript/app/app.js
+++ main/javascript/app/app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../../html/home.html'));
@@ -11,3 +13,26 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+// testing for learning API, nothing matters
+app.get('/home',(req, res) => {
+    res.status(200).send({
+       post: 'Lord of the Ring',
+       author: 'Tolkien'
+    })
+});
+
+app.post('/home/:id',(req, res) =>{
+
+    const { id } = req.params;
+    const { author } = req.body;
+
+    if(!author){
+        res.status(418).send({Message: 'need an author !!!'})
+    }
+
+    res.send({
+        post: `${id} was made by ${author}`,
+    })
+})
---
+++ main/javascript/app/index.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// // const app = express();
+// // const path = require('path');
+// const PORT = process.env.PORT || 3000;
+
+
+// app.get('/home',(req, res) => {
+//      res.status(200).send({
+//         post: 'Lord of the Ring',
+//         author: 'Tolkien'
+//      })
+// });
+
